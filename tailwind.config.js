08bfcd4ff3b44cfba9c50bcea9b183314b1e1c24
Tim Whitacre <tim@timw.co>
2022-08-22 19:22:52 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/*.css",
+    "./public/*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yeller: "#f7f14d",
+        pinker: "#f62fa6",
+      },
+      fontFamily: {
+        sans: ['Jura', 'sans-serif'],       
+      },      
+    },
+  },
+  plugins: [],
+}
